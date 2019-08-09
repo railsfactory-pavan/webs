47800f3bb9b54cc4c1bd0afbac3179cb77364468
railsfactory-pavan <pavanrf07@gmail.com>
2019-08-09 21:42:37 +0530

    complete default value is false

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_09_131044) do
+ActiveRecord::Schema.define(version: 2019_08_09_145206) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
-    t.boolean "complete"
+    t.boolean "complete", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
